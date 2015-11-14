97e931c8118e57e9ca8d0d252f9bb6bddc0d93dd
Roberto Orgiu <roberto.orgiu@gmail.com>
2015-11-14 20:34:18 +0100

    Adds the possibility to show the notification skeleton, so that it can be later completed

--- mobile/src/main/java/it/ennova/aww/MainActivity.java
+++ mobile/src/main/java/it/ennova/aww/MainActivity.java
@@ -1,13 +1,20 @@
 package it.ennova.aww;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Notification;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 /**
  * TIP: http://developer.android.com/training/wearables/notifications/creating.html
  */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Notification notification;
+    private NotificationManagerCompat notificationManager;
+    private final int NOTIFICATION_REQUEST_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +23,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         findViewById(R.id.showNotificationButton).setOnClickListener(this);
         findViewById(R.id.addPageToNotificationButton).setOnClickListener(this);
+
+        notificationManager = NotificationManagerCompat.from(this);
     }
 
     @Override
     public void onClick(View v) {
-        switch(v.getId()) {
+        switch (v.getId()) {
             case R.id.showNotificationButton:
                 addNotification();
                 break;
@@ -34,7 +43,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * Makes the notification
      */
     private void addNotification() {
+        notification = new NotificationCompat.Builder(this)
+                .setContentTitle("Workshop")
+                .setContentText("Wearable Tech Torino")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .build();
 
+        notificationManager.notify(NOTIFICATION_REQUEST_ID, notification);
     }
 
     /**
